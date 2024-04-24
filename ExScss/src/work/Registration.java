664b44da2491ed08c2dfa55bb703ae352fa148d4
yoshitaka.saito <yoshitaka.saito@g.dreamcareer.co.jp>
2024-04-24 16:25:10 +0900

    名前の入力に対してＩＤを生成して返すメソッドの修正

--- ExScss/src/work/Registration.java
+++ ExScss/src/work/Registration.java
@@ -14,11 +14,14 @@ public class Registration {
 		super();
 	}
 
+	/**
+	 * 名前を入力するとIDを生成し、DBへ登録
+	 */
 	public void inputName() {
 		
 		Scanner scanner=new Scanner(System.in);
 		
-		System.out.println("---受験社登録機能---");
+		System.out.println("---受験者登録機能---");
 		System.out.print("登録したい名前を入力してください：");
 		
 		String _name = scanner.next();
@@ -29,7 +32,6 @@ public class Registration {
 		//ユーザーデータ登録
 		int userId=accessdb.setUserNameToGenId(_name);
 		System.out.println(_name+"さんのIDは"+userId+"です。");
-		
 		
 		
 		scanner.close();

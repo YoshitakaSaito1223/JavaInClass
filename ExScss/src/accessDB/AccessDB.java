155d322256ebb809410333eaedd775a8aaff5836
yoshitaka.saito <yoshitaka.saito@g.dreamcareer.co.jp>
2024-04-24 16:23:33 +0900

    名前を入力すると対応する受験番号を返すメソッドの追加

--- ExScss/src/accessDB/AccessDB.java
+++ ExScss/src/accessDB/AccessDB.java
@@ -17,6 +17,11 @@ public class AccessDB {
 	final String CONNECTION = "jdbc:mysql://localhost:3306/java_ex_db?serverTimezone=Asia/Tokyo";
 	final String USER = "root";
 	final String PASSWORD = "password1223";
+	
+	
+	/**
+	 * DB接続テスト用クラス
+	 */
 
 	public void connectDB() {
 
@@ -53,7 +58,11 @@ public class AccessDB {
 		}
 	}
 
-	//名前入力時にID生成
+	/**
+	 * 名前を入力するとIDを返し、それらをDBへ登録
+	 * @param _userName
+	 * @return genId
+	 */
 	public int setUserNameToGenId(String _userName) {
 		String table = "examinee";
 		int genId = 0,rowAffected;
@@ -80,8 +89,9 @@ public class AccessDB {
 			System.out.println("JDBCドライバのロードでエラーが発生しました");
 		} catch (SQLException e) {
 			System.out.println("データベースへのアクセスでエラーが発生しました。");
-			System.out.println(e.getMessage());
-			System.out.println(e.getCause());
+			//error原因の調査用コマンド
+//			System.out.println(e.getMessage());
+//			System.out.println(e.getCause());
 		} finally {
 			try {
 				if (con != null) {
@@ -95,17 +105,76 @@ public class AccessDB {
 		return genId;
 	}
 
-	/*
-	//名前を入力すると試験結果を表示
-	public String getTableDataByName(String _userName) {
-	
+	/**
+	 * 名前を入力するとIDを表示
+	 * @param _userName //ユーザー名
+	 * @return result　//試験結果
+	 */
+	public int[] getIdDataByName(String _userName) {
+		String table = "examinee" ;
+		int i=0,j=0;
+		//int[] result=new int[20];
+
+		try {
+			// JDBCドライバのロード
+			Class.forName(DRIVER);
+			// データベース接続
+			con = DriverManager.getConnection(CONNECTION, USER, PASSWORD);
+			// ひとつ前の番号取得
+			stmt = con.prepareStatement("SELECT id FROM " + table+" WHERE name="+_userName);
+			// 実行結果取得
+			rs = stmt.executeQuery();
+			
+			//帰ってきた値の数をカウント
+			while (rs.next()) 
+				i++;
+			//返り値を格納する配列を用意
+			int[] ids =new int [i+1];
+			
+			//配列に格納
+			while (rs.next()) {
+				ids[j] = rs.getInt("id");
+				j++;
+			}
+			
+			//値を返す
+			return ids;			
+		} catch (ClassNotFoundException e) {
+			System.out.println("JDBCドライバのロードでエラーが発生しました");
+		} catch (SQLException e) {
+			System.out.println("データベースへのアクセスでエラーが発生しました。");
+			//error原因の調査用コマンド
+//			System.out.println(e.getMessage());
+//			System.out.println(e.getCause());
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("データベースへのアクセスでエラーが発生しました。");
+			}
+		}
+		int[] none=new int[2];
+		return none;
+		
 	}
+	
 	
 	//名前とIDを入力すると試験結果を表示
-	public String getTableDataByNameAndId(String _userName, int _id) {
-	
+	public String getResultByNameAndId(String _userName, int _id) {
+		
+		
+		
+		
+		
+		
+		
+		
+		return "未実装";
 	}
 	
+	/*
 	//名前を入力して試験結果を入力
 	public float InsertResultByName(String _userName) {
 	

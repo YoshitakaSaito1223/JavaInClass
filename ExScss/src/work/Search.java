d5c55299c24548e00a6bb52acd493e9e7ee36db5
yoshitaka.saito <yoshitaka.saito@g.dreamcareer.co.jp>
2024-04-26 09:43:08 +0900

    検索ロジックの大枠追加

--- ExScss/src/work/Search.java
+++ ExScss/src/work/Search.java
@@ -9,6 +9,7 @@ public class Search {
 
 	public void searchResult() {
 		int id=0,checkId,sub_id,result_point=0;
+		String sub_name;
 		
 		AccessDB accessdb = new AccessDB();
 
@@ -38,14 +39,15 @@ public class Search {
 		
 		
 		//教科一覧を取得,表示
-		System.out.print("\n受験結果を知りたい教科に対応する数字を入力してください:");
+		System.out.println("\n受験結果を知りたい教科に対応する数字を入力してください:");
 		accessdb.getAllSubjectNameAndId();
 		
 		sub_id=scanner.nextInt();
+		sub_name=accessdb.getSubjectNameBySub_Id(sub_id);
 		
 		//受験者ID,教科IDから結果取得
 		result_point=accessdb.getResultByUserIdAndSubId(id, sub_id);
-		
+		System.out.println(_name+"さんの"+sub_name+"の得点："+result_point);
 		
 		
 		scanner.close();

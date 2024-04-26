a16f885eb6eba199387e5643bafda0fdfa35d2ab
yoshitaka.saito <yoshitaka.saito@g.dreamcareer.co.jp>
2024-04-26 16:06:06 +0900

    体裁を整えた

--- ExScss/src/Main/Exmain.java
+++ ExScss/src/Main/Exmain.java
@@ -26,6 +26,7 @@ public class Exmain {
 		Registration rgstr = new Registration();
 		Search search = new Search();
 		try {
+			System.out.print("機能番号：");
 			int pgNum = scanner.nextInt();
 
 			//機能判定

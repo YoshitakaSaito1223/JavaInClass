8686478f3c49babdc7ceafd9a74b1b4a4b6fcfc4
yoshitaka.saito <yoshitaka.saito@g.dreamcareer.co.jp>
2024-04-30 17:07:47 +0900

    minimun change

--- ExScss/src/work/Admin.java
+++ ExScss/src/work/Admin.java
@@ -12,7 +12,7 @@ public class Admin {
 	Search search = new Search();
 	AccessDB accessdb = new AccessDB();
 	Login login = new Login();
-	
+
 	String admin_name = login.admin_name;
 
 	public void SelectFunction() {
@@ -45,64 +45,61 @@ public class Admin {
 		}
 	}
 
-	public void RegisterResult() {
-		String examinee_name,sub_name,tmp;
-		int examinee_id=0, sub_id=0, result_point=0;
+	private void RegisterResult() {
+		String examinee_name, sub_name, tmp;
+		int examinee_id = 0, sub_id = 0, result_point = 0;
 
-		
 		//登録する名前
 		System.out.print("\n登録したい人の名前もしくはIDを入力してください:");
-		tmp=scanner.nextLine();
-		
+		tmp = scanner.nextLine();
+
 		//名前かIDか判定
 		try {
-			examinee_id=Integer.parseInt(tmp);
-			examinee_name=accessdb.getNameByID(examinee_id);
-		}catch(Exception e) {
-			examinee_name=tmp;
-			ArrayList<Integer> ids=new ArrayList();
+			examinee_id = Integer.parseInt(tmp);
+			examinee_name = accessdb.getNameByID(examinee_id);
+		} catch (Exception e) {
+			examinee_name = tmp;
+			ArrayList<Integer> ids = new ArrayList();
 			ids = accessdb.getIdDataByName(tmp);
-			
+
 			//名前が重複していた場合の処理
-			if(ids.size()>1) {
-				System.out.print(tmp+"さんのIDを入力してください:");
-				int checkId=Integer.parseInt(scanner.nextLine());
-				if(ids.contains(checkId)) {
-					examinee_id=checkId;
-				}else {
+			if (ids.size() > 1) {
+				System.out.print(tmp + "さんのIDを入力してください:");
+				int checkId = Integer.parseInt(scanner.nextLine());
+				if (ids.contains(checkId)) {
+					examinee_id = checkId;
+				} else {
 					System.out.println("IDが間違っています。");
 					System.exit(0);
 				}
-			}else {
-				examinee_id=ids.get(0);
+			} else {
+				examinee_id = ids.get(0);
 			}
 		}
-		
+
 		System.out.print("\n登録したい教科を入力してください：");
-		tmp=scanner.nextLine();
+		tmp = scanner.nextLine();
 		//教科名か教科IDか判定
 		try {
-			sub_id=Integer.parseInt(tmp);
-			sub_name=accessdb.getSubjectNameBySub_Id(sub_id);
-		}catch(Exception e) {
-			sub_name=tmp;
-			sub_id=accessdb.getSubjectIDBySub_Name(tmp);
+			sub_id = Integer.parseInt(tmp);
+			sub_name = accessdb.getSubjectNameBySub_Id(sub_id);
+		} catch (Exception e) {
+			sub_name = tmp;
+			sub_id = accessdb.getSubjectIDBySub_Name(tmp);
 		}
-		
-		
+
 		System.out.print("\n登録する得点を入力してください:");
-		result_point=Integer.parseInt(scanner.nextLine());
-		
+		result_point = Integer.parseInt(scanner.nextLine());
+
 		System.out.println("\n\n登録する情報は以下の情報でよろしいですか？（y/n）\n"
-				+ examinee_name+" - "+sub_name+":"+result_point+"点 \n登録者:"+admin_name);
-		tmp=scanner.nextLine();
-		if(tmp.equals("y") || tmp.equals("Y")) {
+				+ examinee_name + " - " + sub_name + ":" + result_point + "点 \n登録者:" + admin_name);
+		tmp = scanner.nextLine();
+		if (tmp.equals("y") || tmp.equals("Y")) {
 			accessdb.InsertResultByName(examinee_id, sub_id, result_point, admin_name);
 			System.out.println("登録が完了しました。");
-		}else {
+		} else {
 			System.out.println("入力を取り消しました。\n");
 		}
-		
 
 	}
 
